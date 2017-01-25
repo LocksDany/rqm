9b8787767876550bf64cddeec3556de9797d0e99
Dany <LocksDanny@outlook.com>
2017-01-25 20:34:13 -0300

    Random Quote Generator added

--- js/js.js
+++ js/js.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
     $("#gen-button").on('click', function(){
    $.getJSON("/js/json/quotes.json", function(json){
-      $("#gen").html(JSON.stringify(json)); 
+      var html = "";
+       var id = Math.floor(Math.random() * 2);
+       
+       json = json.filter(function(val){
+          return (val.id == id); 
+       });
+       
+       json.forEach(function(val){
+           html += val.quote + "<br>" + "<span class='author'>" + "-" + val.author + "</span>";
+       });
+       
+       $("#gen").html(html);
+       
    });
 });
+    
+    $("#twitter-button").on('click', function(){
+       window.alert("Works!");
+    });
+    
 });
